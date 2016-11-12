a5b78c874404922c89f3787caeb697c6fd0f4783
Aymen Djellal <aymendjellal@gmail.com>
2016-11-11 20:38:15 -0500

    Python 3 git add README.md !

--- README.md
+++ README.md
@@ -4,4 +4,3 @@ Applications multimédias
 Python 2
 x64
 require numpy
-openCV3 (dans C:\builds)
--- project.py
+++ project.py
@@ -1,18 +1,60 @@
 import numpy as np
 import cv2
+video = cv2.VideoCapture("Typing3.mp4")
 
-video = cv2.VideoCapture("Typing1.mp4")
+def show_histogram_color(frame):
+    # Now create a histogram for the frame
+    h = np.zeros((300, 256, 3))
+    b, g, r = cv2.split(frame)
+    bins = np.arange(256).reshape(256, 1)
+    color = [(255, 0, 0), (0, 255, 0), (0, 0, 255)]
 
+    for item, col in zip([b, g, r], color):
+        hist_item = cv2.calcHist([item], [0], None, [256], [0, 255])
+        cv2.normalize(hist_item, hist_item, 0, 255, cv2.NORM_MINMAX)
+        hist = np.int32(np.around(hist_item))
+        pts = np.column_stack((bins, hist))
+        cv2.polylines(h, [pts], False, col)
+
+    h = np.flipud(h)
+#   h = cv2.resize(h,None,fx=4, fy=2, interpolation = cv2.INTER_CUBIC)
+    # Display histogram
+    cv2.imshow('Histogram', h)
+
+
+def show_histogram_black(frame):
+    # Now create a histogram for the frame
+    h = np.zeros((300, 256, 1))
+    bins = np.arange(256).reshape(256, 1)
+
+    hist_item = cv2.calcHist([frame], [0], None, [256], [0, 255])
+    cv2.normalize(hist_item, hist_item, 0, 255, cv2.NORM_MINMAX)
+    hist = np.int32(np.around(hist_item))
+    pts = np.column_stack((bins, hist))
+    cv2.polylines(h, [pts], False, 1)
+
+    h = np.flipud(h)
+#   h = cv2.resize(h,None,fx=4, fy=2, interpolation = cv2.INTER_CUBIC)
+    # Display histogram
+    cv2.imshow('Histogram2', h)
 
 while(True):
     # Capture frame-by-frame
     ret, frame = video.read()
 
+    rows,cols,color = frame.shape
+#    M = cv2.getRotationMatrix2D((cols/2,rows/2),180,1)
+#    frame = cv2.warpAffine(frame,M,(cols,rows))
+
     # Our operations on the frame come here
     gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
     # Display the resulting frame
-    #cv2.imshow('frame',frame)
+    cv2.imshow('frame',gray)
+
+
+    show_histogram_color(frame);
+    show_histogram_black(frame);
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
 
